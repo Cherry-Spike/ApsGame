18a6bed28e592993a3ecbb479e8fe1a22ffdf7f9
Everton Ferreira <everton.ferreira@hotmail.com>
2019-05-02 23:16:00 -0300

    janelas aleatorias (test)

--- src/model/Janela.java
+++ src/model/Janela.java
@@ -4,16 +4,20 @@ import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import view.MainGame;
 
 public class Janela extends Button{
 	
 	protected String JanelaAcesa = "-fx-background-color: transparent; -fx-background-image: url('Model/resources/BotaoAmareloPressionado.png');";
 	protected String JanelaApagada = "-fx-background-color: transparent; -fx-background-image: url('Model/resources/BotaoAmareloSolto.png');";
+	public boolean ativa = true;
 	
-	public Janela() {
-		setPrefWidth(267);
-		setPrefHeight(77);
-		setStyle(JanelaApagada);
+	public Janela(int x, int y) {
+		setPrefWidth(50);
+		setPrefHeight(50);
+		setLayoutX(x);
+		setLayoutY(y);
+		setStyle(JanelaAcesa);
 		InitializerListeners();
 	}
 	
@@ -23,7 +27,8 @@ public class Janela extends Button{
 	}
 	
 	public void SetJanelaApagada() {
-		setStyle(JanelaApagada);		
+		setStyle(JanelaApagada);	
+		ativa = false;
 	}
 	
 	private void InitializerListeners() {
